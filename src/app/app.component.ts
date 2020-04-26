e7dedf299d3b39b21ef11b330a490c80e3e055b7
bpandey3 <bpandey3@hotmail.com>
2020-04-26 11:43:02 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+ value: any;
+
+  constructor() {    
+  }
+
+  ngOnInit() {  
+   this.value=this.formatDate("1:00");
+  }
+
+formatDate(date:string) {
+  var hh = parseInt(date.split(":")[0]);
+  var m = parseInt(date.split(":")[1]);
+  //var s = d.getSeconds();
+  var dd = "AM";
+  var h = hh;
+  if (h >= 12) {
+    h = hh - 12;
+    dd = "PM";
+  }
+  if (h == 0) {
+    h = 12;
+  }
+
+
+  /* if you want 2 digit hours:
+  h = h<10?"0"+h:h; */
+
+  var pattern = new RegExp("0?" + hh + ":" + m + ":");
+
+  var replacement = h + ":" + m;
+  /* if you want to add seconds
+  replacement += ":"+s;  */
+  replacement += " " + dd;
+
+  return replacement;
+}
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+import {
+  CalendarModule
+} from "primeng";
+
+
+@NgModule({
+  imports:      [ 
+    BrowserModule, 
+    BrowserAnimationsModule,
+    FormsModule,
+    CalendarModule,
+     ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
